0a56b2bfa02b1a22b4412db886c48876068ab6de
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-03-31 08:30:09 +0530

    Add: word-break - submission-15

---
+++ Data Structures & Algorithms/word-break/submission-15.py
@@ -0,0 +1,30 @@
+class Solution:
+    def wordBreak(self, s, wordDict):
+        dp = [False] * (len(s)+1)
+        dp[len(s)] = True
+        for i in range(len(s)-1, -1, -1):
+            # print('i', i)
+            for w in wordDict:
+                # print('w', w)
+                if (i+len(w)) <= len(s):
+                    if s[i: i+len(w)] == w:
+                        # print('found match of w', w)
+                        dp[i] = dp[i] or dp[i+len(w)]
+        return dp[0]
+    def wordBreakTopDown(self, s: str, wordDict: List[str]) -> bool:
+        dp = defaultdict(int)
+        dp[len(s)] = True
+        def solve(idx):
+            if idx in dp:
+                return dp[idx]
+            for i in range(idx, len(s)):
+                if s[idx: i+1] in wordDict:
+                    if solve(i+1):
+                        dp[idx] = True
+                        return dp[idx]
+               
+            dp[idx] = False
+            return dp[idx]
+        return solve(0)
+
+
