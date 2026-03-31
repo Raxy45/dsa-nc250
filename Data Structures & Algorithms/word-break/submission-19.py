616502c5a6cf57bdc8e4d76f16684ee168545711
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-03-31 08:34:59 +0530

    Add: word-break - submission-19

---
+++ Data Structures & Algorithms/word-break/submission-19.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = defaultdict(bool)
+        dp[len(s)] = True
+        def solve(idx):
+            if idx in dp:
+                return dp[idx]
+            for i in range(idx, len(s)):
+                if s[idx: i+1] in wordDict:
+                    if solve(i+1):
+                        dp[idx] = True
+                        return dp[idx]
+            dp[idx] = False
+            return dp[idx]
+        return solve(0)
