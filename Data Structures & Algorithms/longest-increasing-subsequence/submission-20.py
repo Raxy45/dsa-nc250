4cbe8f475a6c755bd4421377aea59ec50e802eb7
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-03-31 16:17:17 +0530

    Update: longest-increasing-subsequence - submission-20

--- Data Structures & Algorithms/longest-increasing-subsequence/submission-20.py
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-20.py
@@ -1,5 +1,36 @@
 class Solution:
     def lengthOfLIS(self, nums):
+        ip = []
+        def BS(num):
+            l, r = 0, len(ip)-1
+            print(ip, num)
+            if len(ip)==0:
+                ip.append(num)
+                return
+            if len(ip)>0:
+                if num>ip[-1]:
+                    print('adding num to ip', num, ip)
+                    ip.append(num)
+                    return
+
+            while l<=r:
+                mid = (l+r)//2
+                if ip[mid] == num:
+                    return
+                if num<ip[mid]:
+                    r=mid-1
+                    # print('mid gt than num', ip[mid], num)
+                else:
+                    # print('mid')
+                    l = mid+1
+            print(l, r, num)
+            ip[l] = num
+        for num in nums:
+            BS(num)
+        print(ip)
+        return len(ip)
+            
+    def lengthOfLISBottomUP(self, nums):
         dp = [1] * len(nums)
         for i in range(len(nums)-1, -1, -1):
             for j in range(i+1, len(nums)):
