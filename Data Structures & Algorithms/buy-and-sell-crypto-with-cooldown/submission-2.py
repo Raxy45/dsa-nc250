60c0a0c5552f76a18ea102ad492476022d965ba7
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-04-06 17:29:32 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # At any given point, 3 options:
+        # 1. Buy
+        # 2. Sell
+        # 3. Skip
+        n = len(prices)
+        dp = {}
+        def solve(i, holding):
+            # print(i, holding)
+            if (i, holding) in dp:
+                # print('end reached')
+                return dp[(i, holding)]
+
+            if i>=n:
+                return 0
+
+            if holding:
+                # holding
+                # either sell or skip
+                dp[(i, holding)] = max(prices[i]+solve(i+2, False), solve(i+1, holding))
+                return dp[(i, holding)]
+
+            # not holding
+            # buy or skip
+            dp[(i, holding)] = max(-prices[i]+solve(i+1, True), solve(i+1, holding))
+            return dp[(i, holding)]
+        return solve(0, False)
