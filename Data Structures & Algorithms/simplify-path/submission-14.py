ab06d2e4415f40ee2d19c5b87671d5d1533a6c7b
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-05-23 22:23:48 +0530

    Add: simplify-path - submission-14

---
+++ Data Structures & Algorithms/simplify-path/submission-14.py
@@ -0,0 +1,24 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stk = []
+        i = 0
+        curr = ""
+        while i < len(path):
+            while i<len(path) and path[i] == '/':
+                i += 1
+            
+            curr = ""
+            while i<len(path) and path[i]!= '/':
+                curr += path[i]
+                i += 1
+            
+            if curr == '.':
+                continue
+            
+            if curr == '..':
+                if len(stk) > 0:
+                    stk.pop()
+            elif len(curr) > 0:
+                stk.append(curr)
+        print(stk)
+        return '/' + "/".join(stk)
