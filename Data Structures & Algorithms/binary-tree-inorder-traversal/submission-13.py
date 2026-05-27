cbc4f7f16c69f1ab7e604a31ea462e6b165327d5
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-05-27 08:23:27 +0530

    Add: binary-tree-inorder-traversal - submission-13

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-13.py
@@ -0,0 +1,79 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root):
+        stk = []
+        curr = root
+        lst = []
+        while stk or curr:
+            while curr:
+                stk.append(curr)
+                curr = curr.left
+            node = stk.pop()
+            if node:
+                print(node.val)
+                lst.append(node.val)
+            curr = node.right
+        return lst
+    def inorderTraversalRec(self, root):
+        def dfs(root):
+            if root:
+                dfs(root.left)
+                print(root.val)
+                lst.append(root.val)
+                dfs(root.right)
+        lst = []
+        dfs(root)
+        return lst
+
+
+
+    def inorderTraversalStk(self, root):
+        ans = []
+        curr = root
+        while curr:
+            l_c = curr.left
+            if not l_c:
+                ans.append(curr.val)
+                curr = curr.right
+            else:
+                while l_c.right and l_c.right != curr:
+                    l_c = l_c.right
+                
+                if l_c.right == None:
+                    # we have connection to make
+                    l_c.right = curr
+                    curr = curr.left
+                else:
+                    l_c.right = None
+                    ans.append(curr.val)
+                    curr = curr.right
+        return ans
+
+    def inorderTraversalStk(self, root):
+        stk = []
+        curr = root
+        ans = []
+        while curr or stk:
+            while curr:
+                stk.append(curr)
+                curr = curr.left
+            
+            node = stk.pop()
+            ans.append(node.val)
+            curr = node.right
+        return ans
+    def inorderTraversalRec(self, root: Optional[TreeNode]) -> List[int]:
+        def dfs(node):
+            if not node: return
+            dfs(node.left)
+            ans.append(node.val)
+            dfs(node.right)
+        
+        ans = []
+        dfs(root)
+        return ans
