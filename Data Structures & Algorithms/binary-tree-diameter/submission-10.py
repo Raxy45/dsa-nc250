ca7c3954190fa127033a6bcf358f739aa8b44ab3
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-05-28 09:01:09 +0530

    Add: binary-tree-diameter - submission-10

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-10.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        ans = 0
+        def dfs(root):
+            nonlocal ans
+            if not root: return 0
+            lhs, rhs = dfs(root.left), dfs(root.right)
+            ans = max(ans, lhs+rhs)
+            return 1 + max(lhs, rhs)
+        dfs(root)
+        return ans
+    def diameterOfBinaryTreeComplicated(self, root: Optional[TreeNode]) -> int:
+        ans = 0
+        def dfs(root):
+            nonlocal ans
+            if not root or (not root.left and not root.right): return 0
+            
+            lhs_height, rhs_height = dfs(root.left), dfs(root.right) 
+            curr = 1 + max(lhs_height, rhs_height)
+            if root.left and root.right:
+                ans = max(ans, 1 + lhs_height + 1 + rhs_height, curr)
+            else:
+                ans = max(ans, curr)
+            print('root', root.val)
+            print('lhs height', lhs_height, 'rhs_height', rhs_height)
+            print('ans', ans)
+            print('*****')
+            return curr
+        dfs(root)
+        return ans
+        
