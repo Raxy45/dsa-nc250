6ed1dd92f3279dd03b1612403ac366ddd06517e8
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-05-29 22:38:23 +0530

    Add: binary-tree-maximum-path-sum - submission-18

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-18.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        if not root: return 0
+        ans = float('-inf')
+        def dfs(node):
+            nonlocal ans
+            if not node: return 0
+
+            lhs = max(dfs(node.left), 0)
+            rhs = max(dfs(node.right), 0)
+
+
+            temp = node.val + max(lhs, rhs)
+            # print('loot of lhs, rhs', lhs, rhs, 'for', node.val, 'is', temp)
+            ans = max(ans, node.val + lhs + rhs)
+            return temp
+        dfs(root)
+        return ans
+        
