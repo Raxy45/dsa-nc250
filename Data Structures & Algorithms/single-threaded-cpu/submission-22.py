1b858e1d07ae67418d5b11189681431d745cad7f
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-05-30 20:43:21 +0530

    Add: single-threaded-cpu - submission-22

---
+++ Data Structures & Algorithms/single-threaded-cpu/submission-22.py
@@ -0,0 +1,21 @@
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        q, hp = deque([]), []
+        for i, (enq, prc) in enumerate(tasks):
+            tasks[i].append(i)
+        print(tasks, 'sss')
+        tasks.sort()
+        q = deque(tasks)
+        t = q[0][0]
+        ans = []
+        while q or hp:
+            while q and t>=q[0][0]:
+                enq, prc, i = q.popleft()
+                heapq.heappush(hp, (prc, i))
+            if q and not hp:
+                t = q[0][0]
+            if hp:
+                prc, idx = heapq.heappop(hp)
+                t += prc
+                ans.append(idx)
+        return ans
