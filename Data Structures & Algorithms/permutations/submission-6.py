94f4a441f1e27fc9f0c2cbc1410323151018e962
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-06-01 08:33:47 +0530

    Add: permutations - submission-6

---
+++ Data Structures & Algorithms/permutations/submission-6.py
@@ -0,0 +1,24 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans, st = [], set()
+        def dfs():
+            nonlocal ans
+            if len(subset)==len(nums):
+                ans.append(subset.copy())
+                return ans
+            
+            for i in range(len(nums)):
+                if nums[i]==float('inf'): continue
+                subset.append(nums[i])
+                temp = nums[i]
+                nums[i] = float('inf')
+
+                dfs()
+
+                subset.pop()
+                nums[i] = temp
+        ans, subset = [], []
+        dfs()
+        return ans
+
+        
