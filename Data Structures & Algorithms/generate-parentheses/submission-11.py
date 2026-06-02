8ffaaedf041f08f192b16617f39bb2a8e76f38ad
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-06-02 09:51:35 +0530

    Add: generate-parentheses - submission-11

---
+++ Data Structures & Algorithms/generate-parentheses/submission-11.py
@@ -0,0 +1,21 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def dfs(open, closed):
+            if open+closed == 2*n:
+                ans.append("".join(subset.copy()))
+                return
+            
+            if open<n and open>=closed:
+                subset.append('(')
+                dfs(open+1, closed)
+                subset.pop()
+
+            if open>closed:
+                subset.append(')')
+                dfs(open, closed+1)
+                subset.pop()
+
+        ans, subset = [], []
+        dfs(0,0)
+        print(ans)
+        return ans
