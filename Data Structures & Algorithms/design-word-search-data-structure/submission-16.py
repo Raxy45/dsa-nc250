c96c146f6ccd5668f6a40681c9136fb0883bd919
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-06-06 12:39:33 +0530

    Add: design-word-search-data-structure - submission-16

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-16.py
@@ -0,0 +1,48 @@
+class TrieNode:
+    def __init__(self, char=None, eow=False):
+        self.char = char
+        self.eow = eow
+        self.children = [None] * 26
+
+class WordDictionary:
+
+    def __init__(self):
+        self.parent = TrieNode()
+        self.j=0
+
+    def idx(self, char):
+        return ord(char) - ord('a')
+
+    def addWord(self, word: str) -> None:
+        if not word: return
+        i = 0
+        curr = self.parent
+        while curr and i<len(word):
+            if not curr.children[self.idx(word[i])]:
+                new_node = TrieNode(word[i])
+                curr.children[self.idx(word[i])] = new_node
+            
+            curr = curr.children[self.idx(word[i])]
+            i += 1
+        
+        curr.eow = True
+            
+    def search(self, word: str) -> bool:
+        def search_rec(idx, curr):
+            i = idx
+            while curr and i<len(word):
+                curr_char = word[i]
+                if curr_char !='.':
+                    if not curr.children[self.idx(curr_char)]: return False
+                    curr = curr.children[self.idx(curr_char)]
+                    i += 1
+                    continue
+                # now curr char is '.' -> search all +1 levels of curr
+                for curr_child in curr.children:
+                    if curr_child:
+                        # non None char
+                        if search_rec(i+1, curr_child): return True
+                return False
+            if i==len(word) and curr.eow: return True
+            return False
+        return search_rec(0, self.parent)
