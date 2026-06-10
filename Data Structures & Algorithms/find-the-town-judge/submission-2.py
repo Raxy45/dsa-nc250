674b78c0bbe6d92a17b62cb00d80f96c6f40e408
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-06-10 09:05:53 +0530

    Add: find-the-town-judge - submission-2

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        incoming = defaultdict(list)
+        outgoing = defaultdict(list)
+        for person, trustworthy_person in trust:
+            incoming[trustworthy_person].append(person)
+            outgoing[person].append(trustworthy_person)
+
+        print(incoming)
+        print(outgoing)
+        for i in range(1, n+1):
+            # print(i in outgoing, len)
+            if i not in outgoing and len(incoming[i])==n-1:
+                return i
+                # person trusts no body
+        return -1
+
+        
