fdb4af5495c9f393f523192dde99bf0fdcbe013d
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-06-10 10:44:18 +0530

    Add: clone-graph - submission-6

---
+++ Data Structures & Algorithms/clone-graph/submission-6.py
@@ -0,0 +1,42 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        node_map = {}
+        if not node: return None
+        q = deque([node])
+        curr = None
+        while q:
+            curr = q.popleft()
+            if curr in node_map: continue
+            new_node = Node(curr.val)
+            node_map[curr] = new_node
+            for neighbour in curr.neighbors:
+                q.append(neighbour)
+
+        for og, cp in node_map.items():
+            print(og.val, cp.val)
+        q = deque([node])
+        curr = None
+        visited = set()
+        while q:
+            curr = q.popleft()
+            if curr in visited:
+                continue
+            copied_node = node_map[curr]
+            visited.add(curr)
+            for neighbour in curr.neighbors:
+                print('adding neighbor', neighbour.val,'to', copied_node.val, 'neighbors')
+                copied_node.neighbors.append(node_map[neighbour])
+                q.append(neighbour)
+        return node_map[node]
+            
+            
+
+        
