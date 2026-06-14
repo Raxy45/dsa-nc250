6a53a963494dfd6f8db7899cbab25daad4c4ffb0
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-06-14 20:21:39 +0530

    Add: open-the-lock - submission-7

---
+++ Data Structures & Algorithms/open-the-lock/submission-7.py
@@ -0,0 +1,27 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        current = '0000'
+        deadends = set(deadends)
+        steps = 0
+        
+        q = deque([('0000', 0)])
+        def get_next(num, dir):
+            updated = int(num)+dir+10
+            return str(updated%10)
+                
+        while q:
+            curr, steps = q.popleft()
+            # print('popped', curr, steps)
+            if curr == target: return steps
+            if curr in deadends: continue
+            deadends.add(curr)
+            for i in range(4):
+                # print(curr, i, curr[i])
+                q.append((curr[:i]+get_next(curr[i], +1)+curr[i+1:], steps+1))
+                q.append((curr[:i]+get_next(curr[i], -1)+curr[i+1:], steps+1))
+        return -1
+
+
+
+
+            
