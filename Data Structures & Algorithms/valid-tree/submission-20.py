706932342e13e56a7f070420fd04c4bb880bd4ba
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-06-18 22:45:45 +0530

    Add: valid-tree - submission-20

---
+++ Data Structures & Algorithms/valid-tree/submission-20.py
@@ -0,0 +1,23 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        ans = []
+        visit = set()
+        if len(edges)!=n-1: return False
+
+        graph = [[] for _ in range(n)]
+        for course, pre in edges:
+            graph[course].append(pre)
+            graph[pre].append(course)
+        
+        def solve(course, prev):
+            if course in visit:
+                return
+            
+            visit.add(course)
+            for pre in graph[course]:
+                if pre == prev: continue
+                solve(pre, course)
+                
+        solve(0, -1)
+        return len(visit) == n
+        
