8730b8e0626876968a1a22dd4b053e2afedb9a1a
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-06-18 20:19:49 +0530

    Add: course-schedule-ii - submission-13

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-13.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = {i: [] for i in range(numCourses)}
+        for course, prereq in prerequisites:
+            graph[course].append(prereq)
+        
+        ans = []
+        visited = set()
+        def dfs(curr):
+            if curr not in graph: return True
+            nonlocal ans, visited
+            visited.add(curr)
+            for prereq in graph[curr]:
+                if prereq in visited:
+                    return False
+                if not dfs(prereq):
+                    return False
+            visited.remove(curr)
+            graph.pop(curr)
+            ans.append(curr)
+            return True
+        
+        for i in range(numCourses):
+            if i not in graph: continue
+            if not dfs(i): return []
+        return ans
+                
