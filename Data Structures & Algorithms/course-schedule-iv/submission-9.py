83b7315187e0c56c46e80af874829a612b273eca
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-06-19 19:55:04 +0530

    Add: course-schedule-iv - submission-9

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-9.py
@@ -0,0 +1,31 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        graph = {i: [] for i in range(numCourses)}
+        for pre, course in prerequisites:
+            graph[course].append(pre)
+        
+        prereq_set = defaultdict(set)
+
+        def dfs(course):
+            if course in prereq_set:
+                return prereq_set[course]
+
+            if course not in graph:
+                return []
+            
+            current_prereq_list = set(graph[course])
+            for curr_pre in graph[course]:
+                current_prereq_list.update(dfs(curr_pre))
+            prereq_set[course] = current_prereq_list
+            return prereq_set[course]
+        
+        for i in range(numCourses):
+            dfs(i)
+        
+        ans = []
+        for u, v in queries:
+            if u in prereq_set[v]:
+                ans.append(True)
+            else:
+                ans.append(False)
+        return ans
