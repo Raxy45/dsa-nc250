c5324b967bb6b0dc45c484e6b72a21b36972239c
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-06-23 23:41:23 +0530

    Add: minimum-height-trees - submission-7

---
+++ Data Structures & Algorithms/minimum-height-trees/submission-7.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        if not edges:
+            return [0]
+        graph = defaultdict(set)
+        for a, b in edges:
+            graph[a].add(b)
+            graph[b].add(a)
+        
+        q = deque([])
+        for node, nei in graph.items():
+            if len(nei) == 1:
+                q.append(node)
+        print('leaf nodes', q)
+        while len(q)>2:
+            if len(graph) <=2:
+                break
+            for _ in range(len(q)):
+                node = q.popleft()
+                print('current leaf', node)
+                for nei in graph[node]:
+                    print('removing from', nei)
+                    graph[nei].remove(node)
+                    if len(graph[nei]) == 1:
+                        print('nei became a leaf node', nei)
+                        q.append(nei) 
+                
+                del graph[node]
+        print(graph)
+        return list(graph.keys())
