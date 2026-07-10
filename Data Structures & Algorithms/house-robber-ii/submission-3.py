1f85fd05fe23e4efb556666d0dba1863682bbf20
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-07-10 14:51:55 +0530

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def get_max_loot(start, end):
+            n1, n2 = 0, 0
+            for i in range(start, end, -1):
+                n1, n2 = max(nums[i]+n2, n1), n1
+            return n1
+
+        n = len(nums)-1
+        return max(get_max_loot(n, 0), get_max_loot(n-1, -1))
