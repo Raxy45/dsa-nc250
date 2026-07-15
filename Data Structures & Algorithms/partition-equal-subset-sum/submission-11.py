161a92dafd58a7cbfb5c39c1813fb5e3b9c0790e
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-07-15 08:57:40 +0530

    Add: partition-equal-subset-sum - submission-11

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-11.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canPartition(self, nums):
+        arr_sum = sum(nums)
+        if (arr_sum%2) > 0: 
+            return False
+        req_sum = int(arr_sum/2)
+        n = len(nums)
+        dp = [[False] * (req_sum+1) for _ in range(n+1)]
+
+        for i in range(n+1):
+            dp[i][0] = True
+        
+        for idx in range(n-1, -1, -1):
+            for target in range(req_sum, 0, -1):
+                dp[idx][target] = dp[idx+1][target] 
+                if (target - nums[idx]) >=0 :
+                    dp[idx][target] = dp[idx][target] or dp[idx+1][target-nums[idx]]
+        return dp[0][req_sum]
+    def canPartitionTopDown(self, nums: List[int]) -> bool:
+        arr_sum = sum(nums)
+        if (arr_sum%2) > 0: 
+            return False
+        req_sum = arr_sum/2
+        cache = {}
+        def dfs(idx, curr):
+            if curr == 0: return True
+            if curr <0 or idx==len(nums): return False
+            if (curr, idx) in cache: return cache[(curr, idx)]
+
+            cache[(curr, idx)] = dfs(idx+1, curr-nums[idx]) or dfs(idx+1, curr)
+            return cache[(curr, idx)]
+        return dfs(0, req_sum)
+
+        
