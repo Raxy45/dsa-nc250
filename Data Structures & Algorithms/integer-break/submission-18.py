bb4c852c896923c3fd6e7b82385b8888dc183bdf
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-07-17 23:07:03 +0530

    Add: integer-break - submission-18

---
+++ Data Structures & Algorithms/integer-break/submission-18.py
@@ -0,0 +1,25 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        cache = {0:1}
+        # cache[j] = m
+        # This represents the maximum product you can get to get j
+
+        i = 0
+        def dfs(req_sum):
+            # nonlocal i
+            # print(cache)
+            # print(req_sum)
+            if req_sum in cache: return cache[req_sum]
+            if req_sum < 0: return 0
+            # if i>10: return 0
+
+            curr_mx = float('-inf')
+            for i in range(1, n):
+                curr_mx = max(i * dfs(req_sum - i), curr_mx)
+            cache[req_sum] = curr_mx
+            return cache[req_sum]
+            # i += 1
+
+        return dfs(n)
+
+        
