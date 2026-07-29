d02b15c64229923c979916ecd8579662d3e67792
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-07-29 19:41:59 +0530

    Add: coin-change-ii - submission-8

---
+++ Data Structures & Algorithms/coin-change-ii/submission-8.py
@@ -0,0 +1,28 @@
+class Solution:
+    def change(self, amt, cns):
+        dp = [[0] * (amt+1) for _ in range(len(cns)+1)]
+
+        for i in range(len(cns)+1):
+            dp[i][0] = 1
+
+        # print(dp)
+        for i in range(len(cns)-1, -1, -1):
+            for j in range(1, amt+1):
+                # print(i, cns[i], j)
+                # print(dp)
+                dp[i][j] = dp[i+1][j]
+                if j>= cns[i]:
+                    dp[i][j] += dp[i][j-cns[i]]
+        return dp[0][amt]
+    def changeRec(self, amount: int, coins: List[int]) -> int:
+        dp  = {}
+        def dfs(idx, remaining):
+            if (idx, remaining) in dp: return dp[(idx, remaining)]
+
+            if remaining == 0: return 1
+            if remaining<0 or idx >= len(coins): return 0
+
+            dp[(idx, remaining)] = dfs(idx, remaining - coins[idx]) + \
+                                    dfs(idx+1, remaining)
+            return dp[(idx, remaining)]
+        return dfs(0, amount)
