6a7cb02afa4a118918bd90e66345e66a9cd3bc1b
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-08-02 10:48:21 +0530

    Add: stone-game - submission-15

---
+++ Data Structures & Algorithms/stone-game/submission-15.py
@@ -0,0 +1,15 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        dp = {}
+        def dfs(start, end):
+            if end<start: return 0
+            if start==end:
+                return piles[end]
+            if (start, end) in dp: return dp[(start, end)]
+            
+            curr = float('-inf')
+            dp[(start, end)] = max(curr, piles[start] - dfs(start+1, end), \
+                        piles[end] - dfs(start, end-1))
+            return dp[(start, end)]
+        
+        return True if dfs(0, len(piles)-1) > 0 else False
