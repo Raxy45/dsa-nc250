7be1dd5319a37ebd391560c322912bdba40cc9bf
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-08-02 12:29:04 +0530

    Add: stone-game-ii - submission-1

---
+++ Data Structures & Algorithms/stone-game-ii/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def stoneGameII(self, piles: List[int]) -> int:
+        suffix_sum = [0] * len(piles)
+        curr = 0
+        for i in range(len(piles)-1, -1, -1):
+            suffix_sum[i] = curr+piles[i]
+            curr = suffix_sum[i]
+        def dfs(idx, m):
+            if idx>=len(piles): return 0
+
+            max_delta = float('-inf')
+            curr = 0
+            for i in range(1, (2*m)+1):
+                if (i+idx-1)>=len(piles):
+                    break
+                max_delta = max(max_delta, suffix_sum[idx+i-1]-dfs(idx+i, max(m, i)))
+            return max_delta
+        
+        return dfs(0, 1)
