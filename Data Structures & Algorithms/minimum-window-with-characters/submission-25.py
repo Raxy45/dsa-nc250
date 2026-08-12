36dc06e2514d558617ee4d46905a30934a286885
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-08-12 14:01:10 +0530

    Add: minimum-window-with-characters - submission-25

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-25.py
@@ -0,0 +1,39 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        tmp = Counter(t)
+        n = len(s)
+        smp = {}
+        min_l = float('inf')
+        best_l = 0
+        l, r = 0, 0
+        curr_match = 0
+        while r < len(s):
+            if s[r] not in tmp:
+                r += 1
+                continue
+
+            # s[r] in tmp:
+            if s[r] not in smp:
+                smp[s[r]] = 0
+            smp[s[r]] += 1
+            if smp[s[r]] == tmp[s[r]]:
+                curr_match += 1
+            while curr_match == len(tmp):
+                # all freqs matched, start popping from lhs
+                # if len(s[l: r+1]) < len(ans):
+                if (r-l+1) < min_l:
+                    min_l = r-l+1
+                    best_l = l
+                if s[l] not in tmp:
+                    l += 1
+                    continue
+                smp[s[l]] -= 1
+                if smp[s[l]] >= tmp[s[l]]:
+                    l += 1
+                    continue
+                curr_match -= 1
+                if smp[s[l]] == 0:
+                    smp.pop(s[l])
+                l += 1
+            r += 1
+        return s[best_l:best_l + min_l] if min_l!=float('inf') else ""
