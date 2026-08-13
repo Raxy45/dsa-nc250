bb639ec789fcc2c1a8f7cf1d19b03d56bfb39cd4
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-08-13 10:00:59 +0530

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-26

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-26.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        # I use preorder to pick roots and inorder index ranges to recursively build left and right subtrees in O(n) time.
+        # TC: O(n) -> Each node created once
+        # SC: O(h) -> height of tree -> recursion stk
+        #     log n -> balanced, n -> skewed
+        # Aux: O(h)
+        # Total: O(n)
+        self.pre_index = 0  # pointer in preorder
+        # This preorder index is a global variable and it will move from 0 to n, till your recursion call exits
+
+        # Map value -> index in inorder
+        self.inorder_index = {val:idx for idx, val in enumerate(inorder)}
+
+        def construct(left, right):
+            # left, right are inorder boundaries (inclusive-exclusive)
+            # This condition is when you have left == right
+            if left > right:
+                return None
+
+            # root comes from preorder
+            # This is easy part, just determining root of current split
+            current_root_val = preorder[self.pre_index]
+            current_root = TreeNode(current_root_val)
+            self.pre_index += 1
+
+            # split inorder using hashmap
+            root_index_in_inorder = self.inorder_index[current_root_val]
+
+            # build left and right subtrees
+            # Just a game of index
+            current_root.left = construct(left, root_index_in_inorder-1)
+            current_root.right = construct(root_index_in_inorder+1, right)
+
+            return current_root
+        return construct(0, len(preorder)-1)
+        
