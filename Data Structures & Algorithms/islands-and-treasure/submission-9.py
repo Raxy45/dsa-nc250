c000d8b8ddeb4cceb7eea7dd22818a3b4caa2140
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-08-24 07:53:55 +0530

    Add: islands-and-treasure - submission-9

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-9.py
@@ -0,0 +1,31 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        q = deque([])
+        visited = set()
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j]==0:
+                    q.append((i, j))
+
+        t = 0
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        # print(q)÷
+        while q:
+            # print('b4 t', t, q)
+            # print(visited)
+            for _ in range(len(q)):
+                r, c = q.popleft()
+                if (r, c) in visited:
+                    continue
+                visited.add((r, c))
+                grid[r][c] = t
+                for dr, dc in dirs:
+                    ur, uc = r + dr, c + dc
+                    if min(ur, uc) < 0 or ur==len(grid) or uc==len(grid[0]) or grid[ur][uc]!=2147483647 or (ur, uc) in visited:
+                        # if not (min(ur, uc) < 0 or ur==len(grid) or uc==len(grid[0])):
+                        #     print(grid[ur][uc])
+                        #     print(grid[ur][uc] == 2147483647)
+                        # print('continuing for', ur, uc)
+                        continue
+                    q.append((ur, uc))
+            t += 1
