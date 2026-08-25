54c9d9898712802f5bbc6837e4211b3681455b91
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-08-25 16:08:06 +0530

    Add: interleaving-string - submission-15

---
+++ Data Structures & Algorithms/interleaving-string/submission-15.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s3) != (len(s1) + len(s2)):
+            return False
+        def dfs(si, sj):
+            # print(si, sj)
+            if (si+sj) == len(s3):
+                return True
+            if si==len(s1) and sj==len(s2):
+                return False
+            
+            if si<len(s1) and s1[si] == s3[si+sj]:
+                if (dfs(si+1, sj)):
+                    return True
+
+            if sj<len(s2) and s2[sj] == s3[si+sj]:
+                if dfs(si, sj+1):
+                    return True
+            return False
+        return dfs(0, 0)
