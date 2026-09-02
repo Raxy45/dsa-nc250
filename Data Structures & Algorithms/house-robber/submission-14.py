71cd3efa37bad96efee5ebec81e77bd6af1d4fc0
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-09-02 16:39:31 +0530

    Add: house-robber - submission-14

---
+++ Data Structures & Algorithms/house-robber/submission-14.py
@@ -0,0 +1,23 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n1, n2 = 0, 0
+        for i in range(len(nums)):
+            nums[i] = max(nums[i] + n2, n1)
+            n1, n2 = nums[i], n1
+        return n1
+    def robTopDown(self, nums: List[int]) -> int:
+        cache = {}
+        def dfs(idx):
+            if idx in cache: return cache[idx]
+            if idx >=len(nums): return 0
+            take = nums[idx] + dfs(idx+2)
+            skip = dfs(idx + 1)
+            cache[idx] = max(take, skip)
+            return cache[idx]
+        return dfs(0)
+
+        # At any given point, you will have two options 
+            # take the loot
+            # skip the loot
+        # then return max at each index from the bottom in 
+            # recursive solution, store max at the index in cache
