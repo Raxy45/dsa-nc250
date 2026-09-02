76420cb1a3a3f41fa3ae7167cb885aa12c86dcbc
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-09-02 23:29:22 +0530

    Update: last-stone-weight-ii - submission-12

--- Data Structures & Algorithms/last-stone-weight-ii/submission-12.py
+++ Data Structures & Algorithms/last-stone-weight-ii/submission-12.py
@@ -1,8 +1,47 @@
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
+        # capacity = total_sum / 2
+
+        # For each stone:
+        #     take it if it fits
+        #     OR don't take it
+
+        # Goal:
+        #     maximize the amount we can fill
+        total_sum = sum(stones)
+        req_sum = total_sum // 2
+        dp = {}
+        def dfs(idx, curr_sum):
+            if (idx, curr_sum) in dp:
+                return dp[(idx, curr_sum)]
+
+            if curr_sum == 0:
+                return 0
+            if idx == len(stones):
+                return 0
+            
+            # max(take, not_take) 
+
+            # not_take
+            curr = dfs(idx+1, curr_sum)
+            if stones[idx]<=curr_sum:
+                take = stones[idx] + dfs(idx+1, curr_sum-stones[idx])
+                curr = max(curr, take)
+            dp[(idx, curr_sum)] = curr
+            return dp[(idx, curr_sum)]
+
+        half = dfs(0, req_sum)
+        # print(half)
+        return (total_sum - 2*half)
+            
+        
+class SolutionDifferentApproach:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
         stoneSum = sum(stones)
         target = (stoneSum) // 2
         dp = {}
+
+        # Solution present in book
 
         def dfs(i, total):
             if (i, total) in dp:
