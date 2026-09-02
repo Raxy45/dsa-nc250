f05f9f2ec17b8ee0a5e7fbda646f11b59c5345d1
Yash Salvi <46518495+Raxy45@users.noreply.github.com>
2026-09-02 13:15:45 +0530

    Add: valid-tree - submission-25

---
+++ Data Structures & Algorithms/valid-tree/submission-25.py
@@ -0,0 +1,29 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # Graph is valid tree iff:
+        # 1. Number of edges == n-1
+        # 2. Graph is connected
+        # 3. All the nodes are connected
+
+        # Both 1 and 2 automatically guarantees, we dont get a cycle
+        if len(edges) != n-1:
+            return False
+
+        graph = [[] for _ in range(n)]
+        for u,v in edges:
+            graph[u].append(v)
+            # graph[v].append(u)
+
+        visited = set()
+
+        def dfs(node):
+            if node in visited:
+                return
+            visited.add(node)
+
+            for nei in graph[node]:
+                dfs(nei)
+
+        for i in range(n):
+            dfs(i)
+        return len(visited) == n
